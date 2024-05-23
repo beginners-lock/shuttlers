bbfa6d6f3157c313a24e1a4271c571edc0072754
beginners-lock <chukwurophi@gmail.com>
2024-05-23 02:05:14 +0100

    finished the data call for the students and drivers tables on the admin dashboard

--- src/components/DriversTable.tsx
+++ src/components/DriversTable.tsx
@@ -13,34 +13,29 @@ export default function DriversTable({ keys, drivers }: DriversTableProps){
         <div className="w-full h-[100%]">
             <div className="border-0 border-t-[1px] border-b-[1px] w-full flex flex-row items-center justify-start mb-1" style={{borderColor:ADMINTABLETEXTH, color:ADMINTABLETEXTH}}>
                 <div className="px-1.5 py-0.5 text-sm w-[12%] font-semibold text-left">Date of Reg.</div>
-                <div className="px-1.5 py-0.5 text-sm w-[18%] font-semibold text-center">Name</div>
-                <div className="px-1.5 py-0.5 text-sm w-[18%] font-semibold text-center">Completed Rides</div>
+                <div className="px-1.5 py-0.5 text-sm w-[20%] font-semibold text-center">Name</div>
+                <div className="px-1.5 py-0.5 text-sm w-[14%] font-semibold text-center">Completed Rides</div>
                 <div className="px-1.5 py-0.5 text-sm w-[18%] font-semibold text-center">Drivers Number</div>
-                <div className="px-1.5 py-0.5 text-sm w-[17%] font-semibold text-center">Drivers Mail</div>
+                <div className="px-1.5 py-0.5 text-sm w-[19%] font-semibold text-center">Drivers Mail</div>
                 <div className="px-1.5 py-0.5 text-sm w-[17%] font-semibold text-center">Status</div>
             </div>
 
-            <div className="w-full flex flex-row items-center justify-start" style={{color:NEUTRAL500}}>
-                <div className="px-1.5 py-1.5 text-sm w-[12%] text-left">10/10/2023</div>
-                <div className="px-1.5 py-1.5 text-sm w-[18%] text-center">Sample Name</div>
-                <div className="px-1.5 py-1.5 text-sm w-[18%] text-center">100</div>
-                <div className="px-1.5 py-1.5 text-sm w-[18%] text-center">08123456789</div>
-                <div className="px-1.5 py-1.5 text-sm w-[17%] text-center">samplename@gmail.com</div>
-                <div className="px-1.5 py-1.5 text-sm w-[17%] text-center flex flex-row items-center justify-center">
-                    <div className="w-16 py-0.5 rounded-full" style={{backgroundColor:ACTIVE.bg, color:ACTIVE.color}}>Active</div>
-                </div>
-            </div>
-
-            <div className="w-full flex flex-row items-center justify-start" style={{color:NEUTRAL500}}>
-                <div className="px-1.5 py-1.5 text-sm w-[12%] text-left">10/10/2023</div>
-                <div className="px-1.5 py-1.5 text-sm w-[18%] text-center">Sample Name</div>
-                <div className="px-1.5 py-1.5 text-sm w-[18%] text-center">100</div>
-                <div className="px-1.5 py-1.5 text-sm w-[18%] text-center">08123456789</div>
-                <div className="px-1.5 py-1.5 text-sm w-[17%] text-center">samplename@gmail.com</div>
-                <div className="px-1.5 py-1.5 text-sm w-[17%] text-center flex flex-row items-center justify-center">
-                    <div className="w-16 py-0.5 rounded-full" style={{backgroundColor:INACTIVE.bg, color:INACTIVE.color}}>Inactive</div>
-                </div>
-            </div>
+            {
+                drivers.map((driver, index) => {
+                    return(
+                        <div key={keys[index]} className="w-full flex flex-row items-center justify-start" style={{color:NEUTRAL500}}>
+                            <div className="px-1.5 py-1.5 text-sm w-[12%] text-left">{driver.regdate.split(' ')[1]}</div>
+                            <div className="px-1.5 py-1.5 text-sm w-[20%] text-center">{driver.fullname}</div>
+                            <div className="px-1.5 py-1.5 text-sm w-[14%] text-center">{Object.keys(driver.trips).length}</div>
+                            <div className="px-1.5 py-1.5 text-sm w-[18%] text-center">{driver.phone}</div>
+                            <div className="px-1.5 py-1.5 text-sm w-[19%] text-center">{driver.email}</div>
+                            <div className="px-1.5 py-1.5 text-sm w-[17%] text-center flex flex-row items-center justify-center">
+                                <div className="w-16 py-0.5 rounded-full" style={{backgroundColor:driver.verified?ACTIVE.bg:INACTIVE.bg, color:driver.verified?ACTIVE.color:INACTIVE.color}}>{driver.verified?'Active':'Inactive'}</div>
+                            </div>
+                        </div>
+                    )
+                })
+            }
         </div>
     )
 }
